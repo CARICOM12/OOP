d815432256f6ae1371af547866da7391ba31a9bc
CARICOM12 <0190a@ukr.net>
2017-02-24 23:58:35 +0200

    added base code

---
+++ OOP/OOP-001-commence-of-paradigm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_001_commence_of_paradigm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
