1ce79e3b5fae91f11fd0c642771b1f59121d4026
CARICOM12 <0190a@ukr.net>
2017-02-25 00:39:39 +0200

    A class "User" was created

---
+++ OOP/OOP-001-commence-of-paradigm/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_001_commence_of_paradigm
+{
+    class User
+    {
+        public DateTime BirthDay { set; get; }
+        public int id { set; get; }
+        public string name { set; get; }
+        public string email { set; get; }
+    }
+}
